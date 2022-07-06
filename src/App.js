f5e1b67ddc77663f0fb93b9b852bc5f20c13365d
Digambaran <arjunsreekumar.1@gmail.com>
2022-07-06 14:13:58 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Carry-ui-1 from './Carry-ui-1'
+export default function App() {
+  return (
+    <div>
+      <Carry-ui-1/>
+    </div>
+  )
+}
---
+++ src/Carry-ui-1.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import env from 'env'
+export const Carry-ui-1 = () => {
+  return (
+   <p>Hello from Carry-ui-1 </p>
+  )
+}
+export default Carry-ui-1
